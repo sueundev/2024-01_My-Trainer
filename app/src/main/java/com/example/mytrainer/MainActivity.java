c15c4f47b5bae174e980874327c5a57a4a99d26a
KSE <sueun024@gmail.com>
2024-05-03 16:45:35 +0900

    하단 메뉴 바 작성

--- app/src/main/java/com/example/mytrainer/MainActivity.java
+++ app/src/main/java/com/example/mytrainer/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.mytrainer;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = this.getClass().getSimpleName();
+
+    LinearLayout home_ly;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        init();
+        SettingListener();
+
+        //맨 처음 시작할 탭 설정
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+    }
+
+    private void init() {
+        home_ly = findViewById(R.id.home_ly);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void SettingListener() {
+        //선택 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            if (menuItem.getItemId() == R.id.menu_home) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.home_ly, new homeFragment())
+                        .commit();
+                return true;
+            } else if (menuItem.getItemId() == R.id.menu_exe) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.home_ly, new exeFragment())
+                        .commit();
+                return true;
+            } else if (menuItem.getItemId() == R.id.menu_myPage) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.home_ly, new myPageFragment())
+                        .commit();
+                return true;
+            } else if (menuItem.getItemId() == R.id.menu_record) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.home_ly, new recordFragment())
+                        .commit();
+                return true;
+            } else return false;
+        }
     }
 }
