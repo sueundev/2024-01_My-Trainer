72acccc0f9eeae7a6cb883ef6245af9632257b2d
KSE <sueun024@gmail.com>
2024-05-09 20:20:28 +0900

    기록 부분 - 버튼+화면전환 구현

--- app/src/main/java/com/example/mytrainer/recordFragment.java
+++ app/src/main/java/com/example/mytrainer/recordFragment.java
@@ -1,5 +1,6 @@
 package com.example.mytrainer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class recordFragment extends Fragment {
+    Button exebtn;
+    Button kcalbtn;
+    Button weibtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false);
+        View v = inflater.inflate(R.layout.fragment_record, container, false);
+
+        exebtn = v.findViewById(R.id.exe_btn);
+        kcalbtn = v.findViewById(R.id.kcal_btn);
+        weibtn = v.findViewById(R.id.weight_btn);
+
+        exebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), exe_record_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        kcalbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Kcal_record_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        weibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Weight_record_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
